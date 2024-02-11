c5f2c107fe2000f0c96d7e250dc4e3a45b60857d
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-02-11 14:54:03 +0700

    Update test_two_characters_utils.py

--- Project/tests/test_two_characters_utils.py
+++ Project/tests/test_two_characters_utils.py
@@ -1,1 +1,11 @@
-from two_characters.two_characters_utils import two_characters
+from two_characters.two_characters_utils import alternate
+import unittest
+
+class TwoCharatersTest(unittest.TestCase):
+    def test_give_beabeefeab_should_5(self):
+        s = "beabeefeab"
+        expected_output = 5
+        
+        result = alternate(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
