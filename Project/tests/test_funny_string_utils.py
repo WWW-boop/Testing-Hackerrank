c223f5a3af6bcc8136a7516924b3e8aa91755c5f
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-02-11 12:58:06 +0700

    Update test_funny_string_utils.py

--- Project/tests/test_funny_string_utils.py
+++ Project/tests/test_funny_string_utils.py
@@ -17,3 +17,35 @@ class Funny_String(unittest.TestCase):
         result = funnyString(s)
         
         self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_ivvkxq_should_Not_Funny(self):
+        s = "ivvkxq"
+        expected_output = "Not Funny"
+        
+        result = funnyString(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_ivvkx_should_Not_Funny(self):
+        s = "ivvkx"
+        expected_output = "Not Funny"
+        
+        result = funnyString(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_ovyvzvptyvpvpxyztlrztsrztztqvrxtxuxq_should_Funny(self):
+        s = "ovyvzvptyvpvpxyztlrztsrztztqvrxtxuxq"
+        expected_output = "Funny"
+        
+        result = funnyString(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_uvzxrumuztyqyvpnji_should_Funny(self):
+        s = "uvzxrumuztyqyvpnji"
+        expected_output = "Funny"
+        
+        result = funnyString(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
