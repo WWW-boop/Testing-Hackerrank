13b901e9996619b93d68240293f36cf83d86e3b0
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-02-11 15:07:15 +0700

    Update test_grid_challenge_utils.py

--- Project/tests/test_grid_challenge_utils.py
+++ Project/tests/test_grid_challenge_utils.py
@@ -17,3 +17,19 @@ class FunnyStringTest(unittest.TestCase):
         result = gridChallenge(grid)
         
         self.assertEqual(result,expected_output, f'Should be {expected_output}')
+
+    def test_give_mpxz_abcd_wlmf_should_NO(self):
+        grid = ['mpxz', 'abcd', 'wlmf']
+        expected_output = "NO"
+        
+        result = gridChallenge(grid)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_abc_hjk_mpq_rtv_should_YES(self):
+        grid = ['abc', 'hjk', 'mpq','rtv']
+        expected_output = "YES"
+        
+        result = gridChallenge(grid)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
