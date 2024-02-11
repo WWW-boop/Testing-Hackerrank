671be2cb84f3de5717c837eb4d0cdf48f7c0fc69
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-02-11 12:52:52 +0700

    Update test_funny_string_utils.py

---
+++ Project/tests/test_funny_string_utils.py
@@ -0,0 +1,19 @@
+from funny_string.funny_string_utils import funnyString
+import unittest
+
+class Funny_String(unittest.TestCase):
+    def test_give_acxz_should_Funny(self):
+        s = "acxz"
+        expected_output = "Funny"
+        
+        result = funnyString(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+
+    def test_give_acxz_should_Funny(self):
+        s = "bcxz"
+        expected_output = "Not Funny"
+        
+        result = funnyString(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
