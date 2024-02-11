1a461f6e07851319b9a6e59114644738fe969a3e
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-02-11 14:57:16 +0700

    Update test_two_characters_utils.py

--- Project/tests/test_two_characters_utils.py
+++ Project/tests/test_two_characters_utils.py
@@ -9,3 +9,35 @@ class TwoCharatersTest(unittest.TestCase):
         result = alternate(s)
         
         self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_asdcbsdcagfsdbgdfanfghbsfdab_should_8(self):
+        s = "asdcbsdcagfsdbgdfanfghbsfdab"
+        expected_output = 8
+        
+        result = alternate(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_asvkugfiugsalddlasguifgukvsa_should_0(self):
+        s = "asvkugfiugsalddlasguifgukvsa"
+        expected_output = 0
+        
+        result = alternate(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+    
+    def test_give_a_should_0(self):
+        s = "a"
+        expected_output = 0
+        
+        result = alternate(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
+
+    def test_give_ab_should_2(self):
+        s = "ab"
+        expected_output = 2
+        
+        result = alternate(s)
+        
+        self.assertEqual(result,expected_output, f'Should be {expected_output}')
